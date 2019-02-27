c6c3a78e99ab5d3c19db5793c0591127daded6d5
sxg661 <sxg661@student.bham.ac.uk>
2019-02-27 13:01:23 +0000

    initalials

---
+++ Assets/Scripts/ResultsDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultsDisplay : MonoBehaviour
+{
+    List<VenueInfo> results = new List<VenueInfo>();
+    
+
+    [SerializeField]
+    private GameObject buttonPrefab;
+
+
+
+    private void Awake()
+    {
+        results.Add(new VenueInfo("Joe's Bar"));
+        results.Add(new VenueInfo("James's Bar"));
+        results.Add(new VenueInfo("Sophie's Bar"));
+        results.Add(new VenueInfo("Kai's Bar"));
+        results.Add(new VenueInfo("Thomas's Bar"));
+        results.Add(new VenueInfo("Kat's Bar"));
+        results.Add(new VenueInfo("Kareoke Bar"));
+        results.Add(new VenueInfo("Bar"));
+        results.Add(new VenueInfo("Bar"));
+        results.Add(new VenueInfo("Bar"));
+        results.Add(new VenueInfo("Bar"));
+        results.Add(new VenueInfo("Bar"));
+        results.Add(new VenueInfo("Bar"));
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(VenueInfo result in results)
+        {
+            Instantiate(buttonPrefab, transform, false);
+            buttonPrefab.GetComponent<VenueButtonController>().ChangeName(result.name);
+        }
+    }
+}
---
+++ Assets/Scripts/VenueButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VenueButtonController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text nameText;
+
+
+    public void ChangeName(string name)
+    {
+        nameText.text = name;
+    }
+    
+}
