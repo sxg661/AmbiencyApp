ad6d4ba91e65f60d85db4ff74c5631319cbeb6a9
sxg661 <sxg661@student.bham.ac.uk>
2019-03-11 08:37:59 +0000

    removed unecessary scenes, started writing code for filtered searching

---
+++ Assets/Scripts/SearchScripts/FilteredSearch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilteredSearch : MonoBehaviour
+{
+    private enum ScaleType { REGULAR, LOGARITHMIC}
+
+    [SerializeField]
+    private GameObject lightFill;
+
+    [SerializeField]
+    private GameObject temperatureFill;
+
+    [SerializeField]
+    private GameObject soundFill;
+
+    [SerializeField]
+    private GameObject occpuancyFill;
+
+    [SerializeField]
+    private GameObject humidityFill;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
